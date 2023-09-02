879ff09c9f7e4fd24f995636a339778145fa8395
Htet Yar Zar <43663903+Hashyz@users.noreply.github.com>
2023-09-02 16:52:40 +0630

    Create api.py
    api

---
+++ api.py
@@ -0,0 +1,30 @@
+myProjects = {
+    "hourly":{
+        "Ooredoo":{
+            "HMK":{
+                "link":"https://deepnote.com/workspace/tet-d1f1-9ff5e471-2cda-4f7e-bbf9-c7bb15147850/project/ocadeOoredoo-HMK-2b67e277-ddec-41ff-828a-7ea8bff2a56d/notebook/Notebook%201-f8d59ffbaec74bdaa4ecb3a629bc250b",
+                "api":"Bearer api",
+                "time":1
+                },
+                },
+            },
+    "minutely":{
+      "mytelFarm":{
+        "Bird":{
+            "link":"https://deepnote.com/workspace/chaliq-742e8ea4-5206-47f1-8207-99bde135bfb0/project/MytelBird-7d400993-ae29-4ac5-adbe-ba62b1ff3444/notebook/Notebook%201-85cd3a9dc9d049db82ede5f940934bf8",
+            "api":"Bearer api",
+            "time":15
+                },
+                },
+
+    },
+    "daily":{
+        "playtm":{
+            "Pos":{
+                "link":"https://deepnote.com/workspace/school-8a6d-cf680860-f18e-41d6-ae38-16591df2ae93/project/PlaytmPosition-08829202-21eb-466f-b489-a1246bf98cc4/notebook/Notebook%201-2405de2cfed148a1ac1f56344ce5d9ba",
+                "api":"Bearer api",
+                "time":"08:00"
+                },
+                },
+        }
+}
